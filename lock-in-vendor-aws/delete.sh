c7e5a1171b02fd5abf7f25d3bb73116335d8ede1
Diego Cruz <diego0260203@gmail.com>
2025-06-16 19:28:04 -0300

    add delete script

---
+++ lock-in-vendor-aws/delete.sh
@@ -0,0 +1,9 @@
+
+#!/bin/bash
+set -e
+
+STACK_NAME="python-app-devsecops-stack"
+
+echo "Excluindo stack CloudFormation: $STACK_NAME ..."
+
+aws cloudformation delete-stack --stack-name $STACK_NAME
