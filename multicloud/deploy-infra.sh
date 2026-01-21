898c139254d07e59448e21485d6af4c619e2704c
Diego Cruz <diego0260203@gmail.com>
2026-01-21 08:13:03 -0300

    adding cloudformation deployment step on lockin pipeline

---
+++ multicloud/deploy-infra.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+cd terraform;
+
+# Inicialize o Terraform
+terraform init;
+
+# Valide a configuração
+terraform validate;
+
+# Planeje as mudanças
+terraform plan;
+
+# Aplique a infraestrutura
+time terraform apply -auto-approve;
